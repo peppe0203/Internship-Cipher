3ead1a9d9c0617102789d82afab2aaa9271aa2f5
peppe0203 <1826689collura@zuyd.nl>
2022-05-05 17:01:30 +0200

    Made mvc env

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Internship_Cipher.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
